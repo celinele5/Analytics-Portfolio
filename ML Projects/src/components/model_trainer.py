9a888c0ebe23e5005b9886d3cd6c54e6c5cdeb0e
celinele5 <lele@mail.uc.edu>
2026-02-21 10:04:12 +0700

    Added Hyperparameter Tuning.

--- ML Projects/src/components/model_trainer.py
+++ ML Projects/src/components/model_trainer.py
@@ -49,6 +49,42 @@ class ModelTrainer:
                 "CatBoost Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+
+            params = {
+                "Decision Tree": {
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
+                },
+                "Random Forest": {
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "Gradient Boosting": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    "n_neighbors": [5, 7, 9, 11],
+                },
+                "XGBRegressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "CatBoost Regressor": {
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "iterations": [30, 50, 100],
+                },
+                "AdaBoost Regressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+            }
             
             model_report: dict = evaluate_models(
                 X_train=X_train,
