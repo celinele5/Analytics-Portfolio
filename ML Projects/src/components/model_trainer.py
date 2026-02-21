52e1da20eeca0d8f7d22a526604a377c2dc7418c
celinele5 <lele@mail.uc.edu>
2026-02-21 09:37:26 +0700

    Model Trainer fixed pt 5

--- ML Projects/src/components/model_trainer.py
+++ ML Projects/src/components/model_trainer.py
@@ -21,7 +21,7 @@ from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+    trained_model_file_path = os.path.join('artifacts', "model.pkl")
 
 
 class ModelTrainer:
@@ -59,7 +59,7 @@ class ModelTrainer:
             )
 
             # Get best model score
-            best_model_score = max(model_report.values())
+            best_model_score = max(sorted(model_report.values()))
 
             # Get best model name
             best_model_name = list(model_report.keys())[
@@ -72,7 +72,7 @@ class ModelTrainer:
                 raise CustomException("No best model found.")
 
             logging.info(
-                "Best model found on both training and testing datasets."
+                f"Best model found on both training and testing datasets. Model name: {best_model_name}, Score: {best_model_score}"
             )
 
             save_object(
